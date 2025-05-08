796f4fb283117bf48fcb64207301c4a9c8d12a22
jdotcms <jonathan.sanchez@dotcms.com>
2025-05-08 16:15:04 -0600

    #32065 working on docs

---
+++ src/main/java/com/dotcms/saml/service/handler/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This package groups the handlers to resolve the Assertion coming from the IDP
+ * @see com.dotcms.saml.service.handler.AssertionResolverHandler
+ *
+ * The Handlers to fire the authentication to the IDP (from dotCMS)
+ * @see com.dotcms.saml.service.handler.AuthenticationHandler
+ *
+ * The handlers to resolve the logout to the IDP (from dotCMS)
+ * @see com.dotcms.saml.service.handler.LogoutHandler
+ * @since 1.0
+ * @version 1.2
+ */
+package com.dotcms.saml.service.handler;
